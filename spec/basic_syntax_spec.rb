01412b726b7912bee441655a91375824304c95d0
Anthony Cook <anthonymichaelcook@gmail.com>
2014-03-02 21:44:06 -0600

    Adding some Text tests and making numbers more specific.

--- spec/basic_syntax_spec.rb
+++ spec/basic_syntax_spec.rb
@@ -11,13 +11,32 @@ end
 describe 'Basic Syntax' do
   it 'can set variables' do
     result = FTL.explain 'one: 1'
-    expect(result).to eq "<variable one>: <Number 1>"
+    expect(result).to eq '<variable one>: <Integer 1>'
   end
 
   describe 'Literals' do
-    it 'can interpret single integer literals' do
-      result = FTL.explain '1'
-      expect(result).to eq '<Number 1>'
+    describe 'Numbers' do
+      it 'single number integers' do
+        result = FTL.explain '1'
+        expect(result).to eq '<Integer 1>'
+      end
+
+      it 'multiple number integers' do
+        result = FTL.explain '510'
+        expect(result).to eq '<Integer 510>'
+      end
+    end
+
+    describe 'Text' do
+      it 'single word double quoted strings' do
+        result = FTL.explain '"something'
+        expect(result).to eq '<Text "something">'
+      end
+
+      it 'multi-word double quoted strings' do
+        result = FTL.explain '"something else huh'
+        expect(result).to eq '<Text "something else huh">'
+      end
     end
   end
 end
