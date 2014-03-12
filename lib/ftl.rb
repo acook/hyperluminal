a123d178210b197d32165d9815bf21b6b932666f
Anthony Cook <anthonymichaelcook@gmail.com>
2014-03-11 23:21:34 -0500

    Created FTL namespace stub. Improved executable.

--- lib/ftl.rb
+++ lib/ftl.rb
@@ -1,5 +1,29 @@
-require_relative 'hyperluminal/version'
+require 'bundler'
+
+Bundler.setup
+
+require 'pry'
+
+$ftl_root = Pathname.new(__FILE__).dirname.expand_path
+$LOAD_PATH << $ftl_root.to_s
+
+#dirs = [root.join('explainers', '*.rb'), root.join('nodes', '*.rb')]
+#Dir[*dirs].each {|file| require file }
+
+require 'hyperluminal/version'
 
 module FTL
+  extend self
 
+  def dump file
+    raise NotImplementedError
+  end
+
+  def exe file
+    raise NotImplementedError
+  end
+
+  def explain file
+    raise NotImplementedError
+  end
 end
